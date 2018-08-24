5e27c1798f40855ee406b57705aacae4023f7d76
LiJiacheng <1828055263@qq.com>
2018-08-24 22:55:50 +0800

    add view

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    //
+    public function home(){
+        return '主页';
+    }
+
+    public function help(){
+        return '帮助页';
+    }
+
+    public function about(){
+        return '关于页';
+    }
 }
